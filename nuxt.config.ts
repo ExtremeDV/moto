4da91472d0c08245597e5da4e68300a05112cffe
ExtremeDV <info@extmarket.ru>
2025-06-25 10:58:33 +0300

    fix icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,13 +11,7 @@ export default defineNuxtConfig({
   ],
 
   icon: {
-    clientBundle: {
-      scan: {
-        // note that when you specify those values, the default behavior will be overridden
-        globInclude: ['components/**/*.vue', /* ... */],
-        globExclude: ['node_modules', 'dist', /* ... */],
-      },
-    },
+    serverBundle: 'local',
   },
 
   app: {
