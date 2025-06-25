104a4a4d161320e96d4c1937c6ae2eebb6f3f450
ExtremeDV <info@extmarket.ru>
2025-06-25 20:29:46 +0000

    Deploying to gh-pages from @ ExtremeDV/moto@3d5f1b1cdb0dcfecff062498bc998784b3ca6690 🚀

--- _nuxt/CSDuM5B0.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./BqcJrUv8.js";import e from"./CevMNPDt.js";import{x as n,E as s,h as a,w as i,S as p}from"./BViTonLA.js";import"./BcoM21Dg.js";const d={__name:"ProseNote",setup(c){const o=n();return(r,f)=>(a(),s(e,{color:"info",icon:p(o).ui.icons.info},{default:i(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
