d686ac8503aa6201b7f4bbebd3887c88231a5597
ExtremeDV <info@extmarket.ru>
2025-06-26 10:31:55 +0000

    Deploying to gh-pages from @ ExtremeDV/moto@98a39df483ef0e9552bc446e9f1a42971b40fb4b 🚀

---
+++ _nuxt/0m1YRA-3.js
@@ -0,0 +1,1 @@
+import{r as t}from"./pTD4jCNs.js";import a from"./De7-2fqE.js";import{x as e,E as n,h as s,w as c,S as i}from"./Cv3RHBd7.js";import"./BcoM21Dg.js";const d={__name:"ProseCaution",setup(p){const o=e();return(r,f)=>(s(),n(a,{color:"error",icon:i(o).ui.icons.caution},{default:c(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
---
+++ _nuxt/Db9vHLVE.js
@@ -0,0 +1,1 @@
+import{r as t}from"./pTD4jCNs.js";import e from"./De7-2fqE.js";import{x as n,E as s,h as a,w as i,S as p}from"./Cv3RHBd7.js";import"./BcoM21Dg.js";const d={__name:"ProseNote",setup(c){const o=n();return(r,f)=>(a(),s(e,{color:"info",icon:p(o).ui.icons.info},{default:i(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
