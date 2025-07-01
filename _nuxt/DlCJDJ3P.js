b9877d8f9b4deb6cade2794a9482397869081757
ExtremeDV <info@extmarket.ru>
2025-07-01 10:41:58 +0000

    Deploying to gh-pages from @ ExtremeDV/moto@b514de51abc6871af1fb0188f8eef6ca1c40ed7a 🚀

--- _nuxt/DlCJDJ3P.js
+++
@@ -1,1 +0,0 @@
-import{r as n}from"./DIcMVE6f.js";import a from"./CsFbRK-x.js";import{x as s,E as t,h as e,w as i,S as p}from"./ClulFpI2.js";import"./BcoM21Dg.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(e(),t(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
