cc5e3544ebcc286ebd3c7519cddb8196a69e0449
ExtremeDV <info@extmarket.ru>
2025-07-01 12:29:40 +0300

    text font-size

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -14,7 +14,7 @@ defineProps<{
       color="neutral"
       class="bg-muted/80 backdrop-blur-sm rounded-full px-2 sm:px-4 border border-muted/50 shadow-lg shadow-neutral-950/5"
       :ui="{
-        link: 'text-[14px] px-2 py-1',
+        link: 'text-[7px] px-2 py-1',
         linkLeadingIcon: 'hidden'
       }"
     >
